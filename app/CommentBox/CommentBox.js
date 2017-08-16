e08d8b8e8df64b9d3b4792aa4044609b8ae91738
饕餮 <573789673@qq.com>
2017-08-16 21:11:20 +0800

    初识webpack

--- app/CommentBox/CommentBox.js
+++ app/CommentBox/CommentBox.js
@@ -9,6 +9,8 @@ export default class CommentBox extends React.Component{
             commentArr:[
                 {author:"Lion",body:"this is Lion's comment"},
                 {author:"John",body:"this is John's comment"},
+                {author:"cy",body:"this is John's comment"},
+                {author:"zww",body:"this is John's comment"},
             ]
         }
     }
@@ -18,7 +20,7 @@ export default class CommentBox extends React.Component{
             <div className="comment-box">
                 <h1>Comments</h1>
                 <CommentList commentListContent = {this.state.commentArr}/>
-                <CommentForm/>
+                <CommentForm />
             </div>
         )
     }
@@ -31,18 +33,12 @@ class CommentList extends React.Component{
             commentListContent:props.commentListContent
         }
     };
-
-
     render(){
-        {
-            var data = this.state.commentListContent.map((item,index) => {
-
-                return(
-                    <Comment key={index} author={item.author} body={item.body}/>
-                )
-
-            })
-        }
+        let data = this.state.commentListContent.map((item,index) => {
+            return(
+                <Comment key={index} author={item.author} body={item.body}/>
+            )
+        })
         return(
             <div className="comment-list">
                 {data}
@@ -50,6 +46,7 @@ class CommentList extends React.Component{
         )
     }
 }
+
 //评论列表内的具体内容  作者和评论内容
 class Comment extends React.Component{
     constructor(props){
@@ -68,11 +65,14 @@ class Comment extends React.Component{
 
 //评论撰写
 class CommentForm extends React.Component{
+    constructor(props){
+        super(props)
+    }
 
     render(){
         return(
             <div className="comment-form">
-                <input type="text" placeholder="Your name"/>
+                <input type="text" placeholder="Your name" />
                 <input type="text" placeholder="Your comment"/>
                 <input type="submit" value='submit'/>
             </div>
